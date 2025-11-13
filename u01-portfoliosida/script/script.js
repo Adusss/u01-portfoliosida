58ebf5a2deb3c4cc330a88c21f6fe42b6a9873df
Adrian <79541872+Adusss@users.noreply.github.com>
2025-11-13 09:29:10 +0100

    Delete u01-portfoliosida directory

--- u01-portfoliosida/script/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById("my-links");
-  menu.classList.toggle("show");
-}
-
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
