fa992f8901a1d66114c174dfa8ab67d383b963f9
Adrian <79541872+Adusss@users.noreply.github.com>
2025-11-13 09:28:22 +0100

    Add files via upload

---
+++ u01-portfoliosida/script/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById("my-links");
+  menu.classList.toggle("show");
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
